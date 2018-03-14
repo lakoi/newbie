275b8d0d77051e92f06878faf281e3b7ee746c22
Natdanai Work <nook_nd@hotmail.com>
2018-03-14 16:57:24 +0700

    start project

---
+++ app.js
@@ -0,0 +1,25 @@
+// Parameters
+var sitePath = process.argv[2] || ".";
+var port = 4242;
+
+// Libraries
+var express = require('express');
+var app = express();
+
+// Request logging
+app.use(function(req, res, next) {
+    console.log(req.url);
+    next();
+});
+
+// Start server
+console.log(sitePath);
+console.log("Starting server in: " + __dirname + '/' + sitePath);
+app.use(express.static(__dirname + '/' + sitePath));
+app.listen(port, function() { 
+    console.log("Server running at: http://localhost:" + port)
+});
+
+app.get('/',function(req, res) {
+    res.sendFile(__dirname + '/views/index.html');
+});
